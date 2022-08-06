198aebc816868a9af319d1bffbbf94fb3d3bacb7
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2022-08-06 21:04:01 +0300

    multiline support for secondary_info

--- src/main.ts
+++ src/main.ts
@@ -173,6 +173,9 @@ class TemplateEntityRow extends LitElement {
         #wrapper.hidden {
           display: none;
         }
+        #wrapper .info.pointer .secondary {
+          white-space: pre;
+        }        
         #staging {
           display: none;
         }
